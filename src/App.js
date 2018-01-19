88818996cd1c4da3cc78e361d1b81dbd7fd51b0a
Casper Kuijjer <casper@kuijjer.com>
2018-01-19 15:11:57 +0100

    Added the first steps for apollo-rest-link

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
+import { RestLink } from 'apollo-link-rest';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 
 import GitHubUser from './GitHubUser';
 
+const authenticationHeader = {
+  Authorization: `token ${process.env.REACT_APP_PERSONAL_ACCESS_TOKEN}`
+};
+
+const restLink = new RestLink({
+  uri: 'https://api.github.com',
+  headers: {
+    ...authenticationHeader
+  }
+});
+
+const httpLink = new HttpLink({
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    ...authenticationHeader
+  }
+});
+
 const client = new ApolloClient({
-  link: new HttpLink({
-    uri: 'https://api.github.com/graphql',
-    headers: {
-      Authorization: `token ${process.env.REACT_APP_PERSONAL_ACCESS_TOKEN}`
-    }
-  }),
+  link: ApolloLink.from([
+    restLink,
+    httpLink
+  ]),
   cache: new InMemoryCache()
 });
 
 const App = () => (
   <ApolloProvider client={client}>
-    <GitHubUser login="vijairamcharan" />
+    <GitHubUser login="ckuijjer" />
   </ApolloProvider>
 );
 
