f9a8ddef970dcedee3265d0999407cf1a09a2345
Casper Kuijjer <casper@kuijjer.com>
2018-01-23 15:41:01 +0100

    margin is back!

--- src/GitHubUser.js
+++ src/GitHubUser.js
@@ -3,7 +3,7 @@ import gql from 'graphql-tag';
 
 const GitHubUser = ({ name, login, bio }) => {
   return (
-    <div>
+    <div style={{ marginBottom: 16 }}>
       <div>Name: {name}</div>
       <div>Login: {login}</div>
       <div>Bio: {bio}</div>
